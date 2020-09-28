e52efcb04b8cdc9b6f63807130d2bf2ddf478aeb
Gitea <snowwhite91924@gmail.com>
2020-09-28 23:30:47 +0700

    butter cms

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,11 +4,6 @@
     <div v-else>
       <blog-list :blogs="getBlogs" />
     </div>
-    <!-- <ul>
-      <li v-for="ii in 3" :key="ii">
-        <nuxt-link :to="`/blog/${ii}`">Blog item {{ ii }}</nuxt-link>
-      </li>
-    </ul> -->
   </div>
 </template>
 
